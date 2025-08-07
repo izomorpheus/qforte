f5683cfefb2464184a024f04342ef263e7544697
izomorpheus <izomorphic@gmail.com>
2025-08-07 21:57:06 +0000

    finalized dispatchers (qpu + aer)

--- sandbox/qiskit_api/dispatchers/dispatch_qpu.py
+++ sandbox/qiskit_api/dispatchers/dispatch_qpu.py
@@ -1,7 +1,52 @@
 from qiskit_ibm_runtime import QiskitRuntimeService, Sampler, Estimator
 from qiskit import transpile
+from qiskit.transpiler import generate_preset_pass_manager
 
 class QPUDispatcher:
+    def __init__(self, backend_name=None):
+        self.backend_name = backend_name
+        self.service = QiskitRuntimeService()
+        self.pass_manager = None
+        self.sampler = None
+        self.estimator = None
+        self.backend = self.set_backend_from_name()
+
+    def set_backend_from_name(self):
+        try:
+            self.backend = self.service.least_busy() if not self.backend_name else self.service.backend(backend_name)
+            self.pass_manager = generate_preset_pass_manager(backend=self.backend)
+        except Exception as e:
+            raise ValueError(f"Could not set backend: {e}")
+
+    def get_backend(self):
+        return self.backend
+
+    def set_backend(self, backend_name=None):
+        self.backend_name = backend_name
+        self.set_backend_from_name()
+
+    def dispatch_sampler(self, circuits, shots=None):
+        # Ensure backend is initialized
+        if self.backend is None:
+            self.set_backend_from_name()
+        # Transpile to match hardware connectivity
+        transpiled = transpile(circuits, backend=self.backend)
+        # Instantiate and run sampler
+        self.sampler = Sampler(mode=self.backend)
+        job = self.sampler.run(transpiled, shots=shots)
+        return job.result()
+
+    def dispatch_estimator(self, circuits, observables, precision=None):
+        if self.backend is None:
+            self.set_backend_from_name()
+        transpiled = transpile(circuits, backend=self.backend)
+        pubs = zip(circuits, observables)
+        if self.estimator is None:
+            self.estimator = Estimator(mode=self.backend)
+        job = self.estimator.run(pubs, precision)  # type: ignore
+        return job.result()
+
+class AerDispatcher:
     def __init__(self, backend_name=None):
         self.backend_name = backend_name
         self.service = QiskitRuntimeService()
--- sandbox/qiskit_api/xyz/dispatch_test.py
+++ sandbox/qiskit_api/xyz/dispatch_test.py
@@ -1,38 +1,59 @@
-import os
-import sys
 import matplotlib.pyplot as plt  # type: ignore
+from qiskit.visualization import plot_histogram
 
 # Add the dispatchers directory to the path
-script_dir = os.path.dirname(__file__)
-api_root = os.path.abspath(os.path.join(script_dir, '..'))
-sys.path.insert(0, api_root)
+# script_dir = os.path.dirname(__file__)
+# api_root = os.path.abspath(os.path.join(script_dir, '..'))
+# sys.path.insert(0, api_root)
 
-from dispatchers.dispatch_qpu import QPUDispatcher
+from qforte.qiskit_api.dispatchers import QpuDispatcher
+from qforte.qiskit_api.dispatchers import AerDispatcher
 from qiskit import QuantumCircuit
-from qiskit_ibm_runtime import QiskitRuntimeService
-
 
 def main():
 
-
+    # Create a trivial quantum circuit
     qc = QuantumCircuit(1, 1)
     qc.h(0)
     qc.measure_all()
 
+    # Create a Bell state circuit
     qc_bell = QuantumCircuit(2, 2)
     qc_bell.h(0)
     qc_bell.cx(0, 1)
     qc_bell.measure_all()
 
-    dispatcher = QPUDispatcher()
-    result = dispatcher.dispatch_sampler(circuits=[qc])
-    result_bell = dispatcher.dispatch_sampler(circuits=[qc_bell])
+    # Create dispatchers for QPU and Aer
+    qpu_dispatcher = QpuDispatcher()
+    aer_dispatcher = AerDispatcher()
 
-    counts = result[0].data.meas.get_counts()
-    counts_bell = result_bell[0].data.meas.get_counts()
+    #dispatch the circuits using QPU
+    qpu_result = qpu_dispatcher.dispatch_sampler(circuits=[qc], shots=1024)
+    qpu_result_bell = qpu_dispatcher.dispatch_sampler(circuits=[qc_bell], shots=1024)
 
-    plt.hist(counts)
-    plt.hist(counts_bell)
+    #dispatch the circuits using Aer
+    aer_result = aer_dispatcher.dispatch_sampler(circuits=[qc], shots=1024)
+    aer_result_bell = aer_dispatcher.dispatch_sampler(circuits=[qc_bell], shots=1024)
+
+    #extract data from the QPU results
+    qpu_counts = qpu_result[0].data.meas.get_counts()
+    qpu_counts_bell = qpu_result_bell[0].data.meas.get_counts()
+
+    #extract data from the Aer results
+    aer_counts = aer_result[0].data.meas.get_counts()
+    aer_counts_bell = aer_result_bell[0].data.meas.get_counts()
+    for count in qpu_counts:
+        print(count)
+
+    #plot the qpu results
+    plot1 = plt.hist(qpu_counts.values(), bins=len(qpu_counts), alpha=0.5, label='QPU Single Qubit Circuit')
+    plot2 = plt.hist(aer_counts.values(), bins=len(aer_counts), alpha=0.5, label='QPU Single Qubit Circuit')
+    # plot_histogram(qpu_counts, title="Single Qubit Circuit Counts")
+    # plot_histogram(qpu_counts_bell, title="Bell State Circuit Counts")
+
+    # #plot the aer results
+    # plot_histogram(aer_counts, title="Aer Single Qubit Circuit Counts")
+    # plot_histogram(aer_counts_bell, title="Aer Bell State Circuit Counts")
     plt.show()
 
 if __name__ == "__main__":
