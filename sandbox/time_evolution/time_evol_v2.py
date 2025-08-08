84ccba39af28ce8ad12a94558b998f73329c20ea
izomorpheus <izomorphic@gmail.com>
2025-08-08 23:29:38 +0000

    cleaned up molecule instantiation

--- sandbox/time_evolution/time_evol_v2.py
+++ sandbox/time_evolution/time_evol_v2.py
@@ -12,15 +12,9 @@ mol = qf.system_factory(
     build_type='psi4',  #use psi4
     mol_geometry=geom,  #use the geom we specified
     basis='sto-3g',     #use a minimal basis
-    build_qb_ham = True,# VERY STRANGE BEHAVIOR!!
-    run_fci=1,          #fci energy
-    store_mo_ints=1,    #???
-    build_df_ham=0,     #double factorized hamiltonian
     df_icut=1.0e-6)     #???
 
-dt = 0.5; N = 10; r = 1; order = 1
-
-circuit, computer = trotter_evolve(mol, dt=dt, N=N, r=r, order=order)
+circuit, computer = trotter_evolve(mol, dt=0.1, N=10, r=1, order=1)
 qiskit_circuit = hartree_fock(mol, qforte_to_qiskit(circuit, computer.get_nqubit()))
 workflow = SamplerHistFlow(computer,
                 qiskit_circuit,
