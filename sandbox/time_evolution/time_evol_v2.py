c7a43b766ac7970ab5da9ee5221a90550a9448bc
izomorpheus <izomorphic@gmail.com>
2025-08-09 00:31:40 +0000

    finalized time_evol_v2.py

--- sandbox/time_evolution/time_evol_v2.py
+++ sandbox/time_evolution/time_evol_v2.py
@@ -1,4 +1,4 @@
-import qforte as qf
+from qforte.system import system_factory
 from qforte.qiskit_api.workflows import SamplerHistFlow
 from qforte.qiskit_api.translators import qforte_to_qiskit
 from qforte.evolution.trotter import trotter_evolve, hartree_fock
@@ -8,15 +8,12 @@ geom = [
     ('H', (0., 0., 2.0)),
      ]
 
-mol = qf.system_factory(
-    build_type='psi4',  #use psi4
-    mol_geometry=geom,  #use the geom we specified
-    basis='sto-3g',     #use a minimal basis
-    df_icut=1.0e-6)     #???
+mol = system_factory(
+    build_type='psi4',
+    mol_geometry=geom,
+    basis='sto-3g',
+    df_icut=1.0e-6)
 
 circuit, computer = trotter_evolve(mol, dt=0.1, N=10, r=1, order=1)
 qiskit_circuit = hartree_fock(mol, qforte_to_qiskit(circuit, computer.get_nqubit()))
-workflow = SamplerHistFlow(computer,
-                qiskit_circuit,
-                shots=10000)
-workflow.plot()
+workflow = SamplerHistFlow(computer, qiskit_circuit, shots=10000).plot()
